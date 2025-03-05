0d3cc0ef5d091f9fb4545bc20d1c1aab6dcae042
Prabhudatta Mishra <pmishra4@vclvm179-40.vcl.ncsu.edu>
2025-03-05 15:40:19 -0500

    ADDED LOCK.H with comments

--- h/lock.h
+++ h/lock.h
@@ -1,34 +1,44 @@
 #ifndef _LOCK_H_
 #define _LOCK_H_
 
-#define NLOCKS 50 // Maximum number of locks
+/*This header will serve as my
+global header file for the entire
+reader/writer implementation.*/
 
-// LOCK STATES
-#define LFREE 0
-#define LUSED 1
+#define NLOCKS 50 // Maximum number of locks in my created system
 
-// SPECIAL RETURN FOR DELETED
-#define LDELETED -1
+/*My LOCK states*/
+#define LFREE 0 // LFREE is a free state where the lock is not held by any process
+#define LUSED 1 // LUSED indicates that the lock is held by some process or processes(READ)
 
-// LOCK TYPES
-#define NONE 0
-#define READ 1
-#define WRITE 2
+/*USE of LDELETE is to inform
+processes waiting for the LOCK
+which has been deleted*/
+
+#define LDELETED -1 // giving a special return value for LDELETED
+
+/*My lock Types*/
+
+#define NONE 0  // This is the initial type of the lock when the lock was just created
+#define READ 1  // This is the type of the lock when the lock was acquired by a READER
+#define WRITE 2 // This is the type of the lock when the lock was acquired by a WRITE
+
+/*Creating the structure for my lock_table*/
 
 struct lentry
 {
     int lstate;                // Storing the state of each lock
-    int ltype;                 // Important to define the type of the lock-READ or write
-    int lprio;                 // To store the highest priority among the waiting processes
-    int lqhead;                // Storing the head of the queue of the waiting process
-    int lqtail;                // Storing the tail of the queue of the waiting process
-    int readers_count;         // Number of readers holding this lock
-    int writers_count;         // Number of writers holding this lock
-    int lid;                   // this is an id I am giving to detect duplicacy
-    int lproc_array[NPROC];    // list of processes holding this lock
-    unsigned long wait_return; // returning value for the waiting processes - OK or delete
-    unsigned long ltime;       // Time when the lock was created for debugging
-    int lmax_prio;
+    int ltype;                 // specifies the current type of the lock
+    int lprio;                 // Stores the highest priority among the waiting processes in the queue
+    int lqhead;                // This is the head of the waiting queue
+    int lqtail;                // This stores the tail of the waiting queue
+    int readers_count;         // Stores the total number of readers holding the lock
+    int writers_count;         // I am using this as a flag for having only one writer
+    int lid;                   // This is a unique id for my lock to maintain the instances of a single lock
+    int lproc_array[NPROC];    // List of all processes holding the lock
+    unsigned long wait_return; // stores a return value for the processes waiting for the lock(OK or LDELETED)
+    unsigned long ltime;       // Time when the lock was created, I am using it for debugging
+    int lmax_prio;             // Tracks the maximum scheduling priority among the waiting processes
 };
 
 extern struct lentry lock_tab[NLOCKS];
@@ -38,6 +48,6 @@ extern int lcreate(void);
 extern int ldelete(int lock_descriptor);
 extern int lock(int ldes1, int type, int priority);
 extern int releaseall(int numlocks, int ldes1, ...);
-extern void update_prio_inheritance(int ldes1, int requester_prio);
+extern int update_prio_inheritance(int ldes1, int requester_prio);
 
 #endif
